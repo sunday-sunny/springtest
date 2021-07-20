979cabd1e1b2d64406589e4aa050c35c66f82370
sunday-sunny <mallox22@hanyang.ac.kr>
2021-07-20 13:35:43 +0900

    ch4 머스테치 화면구성

--- src/main/java/com/sunny/springboot/service/posts/PostsService.java
+++ src/main/java/com/sunny/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.sunny.springboot.service.posts;
 
 import com.sunny.springboot.domain.posts.Posts;
 import com.sunny.springboot.domain.posts.PostsRepository;
+import com.sunny.springboot.web.dto.PostsListResponseDto;
 import com.sunny.springboot.web.dto.PostsResponseDto;
 import com.sunny.springboot.web.dto.PostsSaveRequestDto;
 import com.sunny.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,6 +39,21 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 
 }
